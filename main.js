60694de50fe43468bb155449ab010bb1f7ac1b75
judgejab <mattcleary@gmail.com>
2017-05-15 17:54:04 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,59 @@
+import Expo from 'expo';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { TabNavigator, StackNavigator } from 'react-navigation';
+import { Provider } from 'react-redux';
+
+import store from './store';
+import AuthScreen from './screens/AuthScreen';
+import WelcomeScreen from './screens/WelcomeScreen';
+import DeckScreen from './screens/DeckScreen';
+import MapScreen from './screens/MapScreen';
+import ReviewScreen from './screens/ReviewScreen';
+import SettingsScreen from './screens/SettingsScreen';
+
+class App extends React.Component {
+  render() {
+    const MainNavigator = TabNavigator({
+      welcome: { screen: WelcomeScreen },
+      auth: { screen: AuthScreen },
+      main: {
+        screen: TabNavigator({
+          map: { screen: MapScreen },
+          deck: { screen: DeckScreen },
+          review: {
+            screen: StackNavigator({
+              review: { screen: ReviewScreen },
+              settings: { screen: SettingsScreen }
+            })
+          }
+        })
+      }
+    }, {
+      navigationOptions: {
+        tabBar: { visible: false }
+      },
+      lazyLoad: true
+    });
+
+    return (
+      <Provider store={store}>
+        <View style={styles.container}>
+          <MainNavigator />
+        </View>
+      </Provider>
+
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+Expo.registerRootComponent(App);
---
+++ screens/WelcomeScreen.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import _ from 'lodash';
+import { View, Text, AsyncStorage } from 'react-native';
+import { AppLoading } from 'expo';
+import Slides from '../components/Slides';
+
+const SLIDE_DATA = [
+  { text: 'Welcome to JobApp', color: '#03A9F4' },
+  { text: 'Use this to get a job', color: '#009688' },
+  { text: 'Set your location, then swipe away', color: '#03A9F4'  }
+];
+
+class WelcomeScreen extends Component {
+  state : { token: null }
+
+  async componentWillMount() {
+    let token = AsyncStorage.getItem('fb_token');
+
+    if (token) {
+      this.props.navigation.navigate('map');
+      this.setState({ token });
+    }else {
+      this.setState({ token });
+    }
+  }
+
+  onSlidesComplete = () => {
+    this.props.navigation.navigate('auth')
+  }
+
+  render() {
+    if (_.isNull(this.state.token)) {
+      return <AppLoading />;
+    }
+
+    return (
+      <Slides
+        data={SLIDE_DATA}
+        onComplete={this.onSlidesComplete}
+      />
+    );
+  }
+}
+
+export default WelcomeScreen;
